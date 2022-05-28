13c3f5bbacd59f2da420179b0c142036c995afc1
Michael Pennington <mwpennington@live.com>
2022-05-27 22:17:24 -0500

    DONE

--- src/scripts/components/pages/viewAuthor.js
+++ src/scripts/components/pages/viewAuthor.js
@@ -1,28 +1,33 @@
 import clearDom from '../../helpers/clearDom';
 import renderToDOM from '../../helpers/renderToDom';
+import { viewAuthorDetails } from '../../../api/mergedData';
 // import { getBooks, getSingleBook } from '../../../api/bookData';
 
 const viewAuthor = (obj) => {
   clearDom();
   const domString = `
   <div class="text-white ms-5 details">
-  <h5>${obj.authorObject.first_name} ${obj.authorObject.last_name} ${obj.authorObject.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
-  Author Email: <a href="mailto:${obj.authorObject.email}">${obj.authorObject.email}</a>
-  <hr>`;
-  // obj.bookObject.forEach((item) => {
-  //   domString += `
-  //   <div class="text-white ms-5 details">
-  //   <h5>${item.title} by ${item.authorObject.first_name} ${item.authorObject.last_name} ${item.authorObject.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
-  //   Author Email: <a href="mailto:${item.authorObject.email}">${item.authorObject.email}</a>
-  //   <p>${item.description || ''}</p>
-  //   <hr>
-  //   <p>${item.sale ? `<span class="badge bg-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span>
-  //     $${item.price}` : `$${item.price}`}</p>
-  //    </div>
-  //  </div>
-  //   `;
-  // });
-  renderToDOM('#view', domString);
+  <h5>${obj.first_name} ${obj.last_name} ${obj.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
+  Author Email: <a href="mailto:${obj.email}">${obj.email}</a>
+  <hr>
+  <div id="authorsCollection"><div>`;
+  console.warn(viewAuthorDetails('-MiBt86VzkC-EGmJnxjm'));
+  const bookArr = Object.values(obj.bookObject);
+  let bookStr = '';
+  bookArr.forEach((item) => {
+    bookStr += `<div class="text-white ms-5 details">
+    <h5>${item.title} by ${obj.first_name} ${obj.last_name} ${obj.favorite ? '<span class="badge bg-danger"><i class="fa fa-heart" aria-hidden="true"></i></span>' : ''}</h5>
+    Author Email: <a href="mailto:${obj.email}">${obj.email}</a>
+    <p>${item.description || ''}</p>
+    <hr>
+    <p>${item.sale ? `<span class="badge bg-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span>
+      $${item.price}` : `$${item.price}`}</p>
+     </div>
+   </div>
+    `;
+  });
+  renderToDOM('#view', domString, bookStr);
+  renderToDOM('#authorsCollection', bookStr);
 };
 
 export default viewAuthor;
